4af35bcae3cdc34e896229e86c5f4ef69a52b684
sujeet kumar <kumarsujeet5565@gmail.com>
2022-11-24 19:08:36 +0530

    AdminLoginAndSetup

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('admin/layout')
+
+@section('container')
+    <div class="row">
+        <h2>Dashboard</h2>
+    </div>   
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('admin',[AdminController::class, 'index']);
+Route::post('admin/auth',[AdminController::class, 'auth'])->name('admin.auth');
+
+
+Route::group(['middleware'=>'admin_auth'], function (){
+    Route::get('admin/dashboard',[AdminController::class, 'dashboard']);
+    Route::get('admin/category',[CategoryController::class, 'index']);
+
+    Route::get('admin/manage_category',[CategoryController::class, 'manage_category']);
 });
+
